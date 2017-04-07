3100547709ee4a5ae47dd9ead668ab6275878784
George Chacko <chackoge@gmail.com>
2017-04-07 17:15:22 -0500

    fixed bug in as.integer step of ae2

--- assembly/alemtuzumab_assembly/alemtuzumab_merge.R
+++ assembly/alemtuzumab_assembly/alemtuzumab_merge.R
@@ -34,10 +34,36 @@ alem_pmid1_2 <- read.csv("alem_rev.csv",stringsAsFactors=FALSE)
 alem_merge1 <-rbind(alem_ct1,alem_ct2,alem_patent1,alem_patent2,alem_fda1,alem_fda2,
 alem_pmid1_1,alem_pmid1_2)
 alem_merge1 %>% arrange(desc(stype)) %>% unique()
-alem_pmid1_for_eric <- alem_merge1 %>% filter(ttype=="pmid1") %>% 
-select(target) %>% unique()
 write.csv(alem_merge1,file="alem_merge1.csv")
-write.csv(alem_pmid1_for_eric,file="alem_pmid1_for_eric.csv")
+
+alem_eric1 <- alem_merge1 %>% filter(ttype=="pmid1") %>% 
+select(target) %>% unique()
+
+ae1 <- alem_eric1[1:400,]
+ae1 <- as.integer(ae1)
+ae2 <- alem_eric1[401:length(alem_eric1$target),]
+ae2 <- as.integer(ae2)
+
+library(rentrez)
+ae1_1 <- entrez_summary(db="pubmed",id=ae1)
+Sys.sleep(30)
+ae2_1 <- entrez_summary(db="pubmed",id=ae2)
+source ("~/NETELabs_CaseStudies/assembly/ericformat.R")
+
+ae1_2 <- ericFormat(ae1_1)
+ae2_2 <- ericFormat(ae2_1)
+
+alem_eric_stage1 <- rbind(ae1_2,ae2_2)
+write.csv(alem_eric_stage1,file="alem_eric_stage1.csv")
+
+
+
+
+
+
+
+
+
 
 
 
