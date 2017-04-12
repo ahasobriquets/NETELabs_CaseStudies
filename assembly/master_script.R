d24b483502edcfecf491df3d5a27661459504083
George Chacko <chackoge@gmail.com>
2017-04-12 11:28:01 -0400

    initial version of master script

---
+++ assembly/master_script.R
@@ -0,0 +1,32 @@
+# Assembles data for all five drugs by calling individual scripts consecutively
+
+# Run load_citation. TThis script flattens Eric Livingston's data to generate
+# a list of pmid1s of cited references derived from reviews of relevant literature
+
+source("~//NETELabs_CaseStudies/Review_Master/load_core_citation_data.R")
+
+# Run alemtuzumab_merge.R
+source("~/NETELabs_CaseStudies/assembly/alemtuzumab_assembly/alemtuzumab_merge.R")
+
+# Run imatinib_merge.R
+source ("~/NETELabs_CaseStudies/assembly/imatinib_assembly/imatinib_merge.R")
+
+# Run nelarabine_merge.R
+
+# Run ramucirumab_merge.R
+
+# Run sunitinib_merge.R
+
+
+
+
+
+
+
+
+
+
+
+
+
+
