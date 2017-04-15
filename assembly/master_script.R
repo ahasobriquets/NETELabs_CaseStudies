1b84df22cf8283f349da9a72c0a62820c3626b82
George Chacko <chackoge@gmail.com>
2017-04-15 11:48:59 -0400

    updated master script

--- assembly/master_script.R
+++ assembly/master_script.R
@@ -5,20 +5,28 @@
 
 source("~//NETELabs_CaseStudies/Review_Master/load_core_citation_data.R")
 
+# Run pazdur.R Assembles cited references from FDA Approval Summary papers
+
+source("~//NETELabs_CaseStudies/Review_Master/pazdur.R")
+
 # Run alemtuzumab_merge.R
-source("~/NETELabs_CaseStudies/assembly/alemtuzumab_assembly/alemtuzumab_merge.R")
+source("~/NETELabs_CaseStudies/assembly/alemtuzumab_merge.R")
 
 # Run imatinib_merge.R
-source ("~/NETELabs_CaseStudies/assembly/imatinib_assembly/imatinib_merge.R")
+source ("~/NETELabs_CaseStudies/assembly/imatinib_merge.R")
 
 # Run nelarabine_merge.R
-source ("~/NETELabs_CaseStudies/assembly/nelarabine_assembly/nelarabine_merge.R")
+source ("~/NETELabs_CaseStudies/assembly/nelarabine_merge.R")
 
 # Run ramucirumab_merge.R
-source ("~/NETELabs_CaseStudies/assembly/ramucirumab_assembly/ramucirumab_merge.R")
+source ("~/NETELabs_CaseStudies/assembly/ramucirumab_merge.R")
 
 # Run sunitinib_merge.R
-source ("~/NETELabs_CaseStudies/assembly/sunitinib_assembly/sunitinib_merge.R")
+source ("~/NETELabs_CaseStudies/assembly/sunitinib_merge.R")
+
+# Run metadata.R - collects edge counts by node type
+source ("~/NETELabs_CaseStudies/assembly/metadata.R")
+
 
 
 
