4825fdeb94d493c2828373788fef5c0b20a1706a
George Chacko <chackoge@gmail.com>
2017-04-18 20:10:59 -0400

    fixed bug

--- assembly/alemtuzumab_merge.R
+++ assembly/alemtuzumab_merge.R
@@ -30,7 +30,6 @@ ttype="pmid1") %>% select(source,stype,target,ttype)
 
 # Root to nct_derived clinical trials
 alem_ct_nct <- read.csv("alemtuzumab_ct_nct.csv",stringsAsFactors=FALSE)
-
 alem_ct3 <- alem_ct_nct %>% mutate(source="alemtuzumab",stype="root",
 target=paste(pmid,"_ct",sep=""),ttype="ct") %>% select(source,stype,target,ttype)
 # nct to pmid1
@@ -60,11 +59,10 @@ alem_pmid1_1 <- alem_pmid1 %>% mutate(source="alemtuzumab",stype="root",
 target=pmid,ttype="pmid1") %>% select(source,stype,target,ttype)
 # reads csv file from load_core_citation_data script run at beginnning
 alem_pmid1_2 <- read.csv("alem_rev.csv",stringsAsFactors=FALSE)
-alem_pmid1_2 <- alem_pmid1_2 %>% mutate(source="alemtuzumab",stype="root",
-target=pmid,ttype="pmid1") %>% select(source,stype,target,ttype)
 
 ## Merge all components
-alem_merge1 <-rbind(alem_ct1,alem_ct2,alem_ct3,alem_ct4,alem_fda1,alem_fda2,alem_fda3,alem_patent1,alem_patent2,alem_pmid1_1,alem_pmid1_2)
+alem_merge1 <-rbind(alem_ct1,alem_ct2,alem_ct3,alem_ct4,alem_fda1,alem_fda2,alem_fda3,
+alem_patent1,alem_patent2,alem_pmid1_1,alem_pmid1_2)
 alem_merge1 %>% arrange(desc(stype)) %>% unique()
 write.csv(alem_merge1,file="alem_merge1.csv")
 
@@ -74,15 +72,14 @@ select(target) %>% unique()
 alem_eric1 <- na.omit(alem_eric1)
 print(dim(alem_eric1))
 
-## Format list per Eric's specs using rentrez in chunks
+## Format list per Eric's specs using rentrez in chunks	
 ae1 <- alem_eric1[1:400,]
-ae1 <- as.integer(ae1)
 ae2 <- alem_eric1[401:length(alem_eric1$target),]
+ae1 <- as.integer(ae1)
 ae2 <- as.integer(ae2)
 
 library(rentrez)
 ae1_1 <- entrez_summary(db="pubmed",id=ae1)
-Sys.sleep(120)
 ae2_1 <- entrez_summary(db="pubmed",id=ae2)
 
 # Call Custom Function to build data frame from list
